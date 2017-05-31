005fea64c0ca75d3867297604b0f57698db2117f
yves <lemee.yves0864@hotmail.fr>
2017-05-31 22:03:52 +0200

    3f66b626d728dc49ee462d67e8c5a29aeee24ff9

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
 <body>
 	<h1>Test de GIT</h1>
 	<h2>Test des commit</h2>
-	<h3>Test des commit 2</h3>
-	<p>test yep</p>
+	<h3>Test des commit</h3>
 	<script type="text/javascript" src="common.js"></script>
 </body>
 </html>
