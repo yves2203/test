7c95f1c7d1d34bb3fa1690e268548c6c6c395b21
yves <lemee.yves0864@hotmail.fr>
2017-05-31 21:25:54 +0200

    commit global

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Titre de la page</title>
+</head>
+<body>
+	<h1>Test de GIT</h1>
+	<h2>Test des commit</h2>
+	<h3>Test des commit 2</h3>
+</body>
+</html>
