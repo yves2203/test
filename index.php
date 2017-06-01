cebf72eeed8e1d9b7a1a0d320021abc2ebb71c44
yves <lemee.yves0864@hotmail.fr>
2017-06-01 18:23:52 +0200

    change index.php sur features/front

--- index.php
+++ index.php
@@ -8,6 +8,17 @@
 	<h1>Test de GIT</h1>
 	<h2>Test des commit</h2>
 	<h3>Test des commit</h3>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+	<p>
+	Nouvelle branche
+	</p>
 	<script type="text/javascript" src="common.js"></script>
 </body>
 </html>
